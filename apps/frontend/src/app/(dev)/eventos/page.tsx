1ffade06376f65bc57f63214a7959f942cfffe17
wesleymoraesconecta <131008751+wesleymoraesconecta@users.noreply.github.com>
2024-11-19 14:11:21 -0300

    feat: qr code

--- apps/frontend/src/app/(dev)/eventos/page.tsx
+++ apps/frontend/src/app/(dev)/eventos/page.tsx
@@ -2,6 +2,7 @@ import { eventos } from "@/core";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import QRCode from "react-qr-code";
 
 export default function PaginaEventos() {
   return (
@@ -20,9 +21,15 @@ export default function PaginaEventos() {
                 className="object-cover"
               />
             </div>
-            <div className="flex flex-1 flex-col gap-2 p-7">
+            <div className="flex flex-1 flex-col items-center gap-2 px-7 py-5">
               <span className="text-lg font-black">{evento.nome}</span>
-              <p className="flex-1 text-sm text-zinc-400">{evento.descricao}</p>
+              <p className="flex-1 text-center text-sm text-zinc-400">
+                {evento.descricao}
+              </p>
+              <QRCode
+                value={JSON.stringify({ id: evento.id, senha: evento.senha })}
+                className="h-44 w-44"
+              />
             </div>
             <div className="flex flex-1 items-center gap-5 p-2">
               <Link
