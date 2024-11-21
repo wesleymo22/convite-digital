b0c308823a815bb34d3600a3220900b83bff2a8b
wesleymoraesconecta <131008751+wesleymoraesconecta@users.noreply.github.com>
2024-11-21 10:40:42 -0300

    feat: configurando prisma

--- packages/core/src/shared/Id.ts
+++ packages/core/src/shared/Id.ts
@@ -1,9 +1,7 @@
+import  { v4 as uuid} from 'uuid'
+
 export default class Id {
   static novo(): string {
-    return `${this.hash()}-${this.hash()}-${this.hash()}`;
-  }
-
-  private static hash(): string {
-    return Math.random().toString(36).substring(2, 15);
+    return uuid()
   }
 }
